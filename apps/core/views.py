987a4afd14f40b2922e8a57b5e57288f76f46b96
Chris Moe <chris.moe@gmail.com>
2020-04-30 12:20:33 -0700

    trying to merge

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+import datetime
 
 from apps.core.helpers import get_book_cover_url_from_api, redirect_back
 from apps.core.models import Contact
-from apps.core.forms import AddContactForm
+from apps.core.forms import AddContactForm, EditContactForm
 from django.db.models.functions import Lower
+
+from django.core.paginator import Paginator
 
 
 def contact_home(request):
     contact_lists = Contact.objects.all().order_by(Lower('last_name'))
+    paginator = Paginator(contact_lists, 5)
     context = {
         'all_contact_lists': contact_lists,
     }
-    return render(request, 'pages/home.html', context)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'pages/home.html', {'page_obj': page_obj})
 
 def contact_details(request, contact_id):
     contact_list_requested = Contact.objects.get(id=contact_id)
@@ -32,35 +38,68 @@ def contact_create(request):
             contact.save()
             return redirect('/')
         else:
-            form = AddContactForm()
-        context = {
-            'form': form,
-        }
-        return render(request, 'pages/form_page.html', context)
-            # Contact.objects.create(
-            #     first_name=form.cleaned_data['first_name'],
-            #     last_name=form.cleaned_data['last_name'],
-            #     email=form.cleaned_data['email'],
-            #     picture=form.cleaned_data['picture'],
-            #     phone=form.cleaned_data['phone'],
-            #     address=form.cleaned_data['address'],
-            #     birthday=form.cleaned_data['birthday'],
-            #     notes=form.cleaned_data['notes'],
-            #     type=form.cleaned_data['type'],
-            #     frequency=form.cleaned_data['frequency'],
-            #     linkedin=form.cleaned_data['linkedin'],
-            #     facebook=form.cleaned_data['facebook'],
-            #     instagram=form.cleaned_data['instagram'],
-            #     twitter=form.cleaned_data['twitter'],
-            #     creator_user=logged_in_user,
-            # )
-            # return redirect('/')
+            context = {
+                'form': form,
+            }
+            return render(request, 'pages/form_page.html', context)
     else:
         form = AddContactForm()
     context = {
         'form': form,
     }
     return render(request, 'pages/form_page.html', context)
+
+
+# def friend_list_create_book(request, friend_id):
+#     friend_list_requested = Contact.objects.get(id=friend_id)
+#     if request.method == 'POST':
+#         # Create a form instance and populate it with data from the request
+#         form = AddBookForm(request.POST)
+#         if form.is_valid():
+
+#             url = get_book_cover_url_from_api(form.cleaned_data['title'])
+
+#             Book.objects.create(
+#                 title=form.cleaned_data['title'],
+#                 description=form.cleaned_data['description'],
+#                 # SOLUTION:
+#                 friend_list=friend_list_requested,
+#                 cover_url=url,
+#             )
+#             # Redirect back to the reading list we were at
+#             return redirect('/list/' + str(friend_list_requested.id) + '/')
+#     else:
+#         # if a GET  we'll create a blank form
+#         form = AddFriendForm()
+#     context = {
+#         'form': form,
+#     }
+#     return render(request, 'pages/form_page.html', context)
+
+def edit_contact(request, contact_id):
+    # Get the contact we are looking for
+
+    contact_to_edit = Contact.objects.get(id=contact_id)
+
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request
+        form = EditContactForm(request.POST, request.FILES, instance=contact_to_edit)
+
+        if form.is_valid():
+            form.save()
+            print('this is a test')
+            return redirect('/contact/'+str(contact_id))
+
+    else:
+        # A GET, create a pre-filled form with the instance.
+        form = EditContactForm(instance=contact_to_edit)
+        print('this is a different test')
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'pages/edit_contact.html', context)
 
 
 @login_required
@@ -151,4 +190,58 @@ def contact_delete(request, contact_id):
 #         friend_list.users_who_voted.add(logged_in_user)
 #         friend_list.save()
 #     return redirect_back(request)
+
+# # Psuedo code
+# if New friend added to friend_list, 
+# Set reminder to contact friend in 7 days
+# if snooze is selected, set reminder to contact friend in 24 hours
+
+# @login_required
+# def contact_friend(request, friend_id):
+#     new_friend = friend_list.objects.get(id=list_id)
+
+
+# @login_required
+# @login_required
+# def user_dashboard(request):
+#     context={
+
+#     }
+#     return render(request, 'pages/dashboard.html', context)
+# @login_required
+# def snooze(request, friend_id):
+    
+
+#     return redirect('/')
+
+# # @login_required
+# def hi_friends(request):
+#     if request.method == 'GET':
+#         contact_lists = Contact.objects.all().order_by(Lower('last_name'))
+#         for contact_lists:
+#             reminder = contact_lists.frequency
+#             x = reminder
+#             contact_date = x - 1
+#             if contact_date == 0:
+#  context = {
+#         'contact_list': contact_lists,
+#         # 'all_friends': contacts,
+#     }
+#     return render(request, 'pages/home.html', context)
+
+# # look at contact method
+# def snooze(request, contact_id): 
+#     Contact.frequncy += 1
+
+# look at twitten (like the editing is snooze)
+
+# def hi_friends(request):
+#     if request.method == 'GET':
+#         contact_list = Contact.objects.all().order_by(Lower('last_name'))
+
+#         if contact_list.frequency
+
+
+             
+        
 
