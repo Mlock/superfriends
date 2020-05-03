a69634d3dddfbb7694c98eb7f7be149c6c710931
jMc <jeanette.c.mack@gmail.com>
2020-05-03 16:41:33 -0700

    changes to views and user page to allow access to countdown on user page

--- apps/core/views.py
+++ apps/core/views.py
@@ -32,7 +32,7 @@ def contact_home(request):
 
 # the new view finishes with a redirect to make button work- this one is "contacted" - jMc
 @login_required
-def update_contacted(request, contact_id): # , frequency modified
+def update_contacted(request, contact_id):
     contact = Contact.objects.get(id=contact_id)
 
     if contact.creator_user == request.user:
@@ -41,32 +41,25 @@ def update_contacted(request, contact_id): # , frequency modified
 
     return redirect('/')
 
-
-    
-    #After performing some kind of operation with side effects, like creating or deleting an object, it’s a best practice to redirect to another URL to prevent accidentally performing the operation twice.
-
-# the new view finishes with a redirect to make button work- this one is "snooze" - jMc
+# TODO the new view finishes with a redirect to make button work- this one is "snooze" - jMc
 # def home_set_snooze(request):
 
 def user_page(request):
     contacts = Contact.objects.order_by(Lower('last_name'))
     contacts_by_user = contacts.filter(creator_user=request.user.id)
     countdowns = [get_contact_countdown(c) for c in contacts_by_user] # add countdown info 
-    # this is about what you need on the template to make this work:
-    # {% for contact, countdown in filtered_countdowns %}
-    # because zip returns a tuple
-
     paginator = Paginator(contacts_by_user, 5)
-
-    context = {
-        'contacts': contacts_by_user,
-        'countdowns': zip(contacts_by_user, countdowns),
-        'user_on_page': request.user.id,
-    }
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'pages/user_page.html', {'page_obj': page_obj})
+    context = {
+        'contacts': zip(contacts_by_user, countdowns),
+        'countdowns': countdowns,
+        'user_on_page': request.user.id,
+        'page_obj': page_obj,
+    }
+    
+    return render(request, 'pages/user_page.html', context)
 
 
 def contact_details(request, contact_id):
@@ -129,15 +122,13 @@ def contact_delete(request, contact_id):
         contact.delete()
     return redirect('/')
 
-
-
-# jMc bgin
-# converts the string that was stored in the DB to a time delta
 # TODO frequency is likely best kept as an integer that gets converted to a time delta
 # and then there is an alternative option to make the next contact an exact date with a calendar picker
 # note: timedelta does not have values for year or month. As for Django DurationField, "on all databases 
 #       other than PostgreSQL, comparing the value of a DurationField to arithmetic on 
-#       DateTimeField instances will not work as expected", so I went with vanilla Python
+#       DateTimeField instances will not work as expected", so I went with vanilla Python - jMc
+
+# converts the string that was stored in the DB to a time delta - jmc
 def get_interval (frequency):    
     conversions = {
         'daily' : timedelta(days=1),
@@ -149,17 +140,17 @@ def get_interval (frequency):
     }
     return conversions[frequency]
 
-# for when intervals are stored as integers
-def set_interval (frequency):
-    conversions = {     
-        'daily' : 1,
-        'weekly' : 7,
-        'monthly' : 30,
-        'quarterly' : 90,
-        'yearly' : 365,
-        'custom' : 14, # arbitrary number until this is worked out
-    }
-    return conversions[frequency]
+# TODO for when intervals are stored as integers
+# def set_interval (frequency):
+#     conversions = {     
+#         'daily' : 1,
+#         'weekly' : 7,
+#         'monthly' : 30,
+#         'quarterly' : 90,
+#         'yearly' : 365,
+#         'custom' : 14, # arbitrary number until this is worked out
+#     }
+#     return conversions[frequency]
 
 # this function can be used to display countown; it's being used in the filter - jMc
 def get_contact_countdown (contact):
@@ -168,37 +159,21 @@ def get_contact_countdown (contact):
     contact_countdown = (get_interval(contact.frequency) + reference_date) - datetime.now(timezone.utc) #will be frequency_modified once DB is updated 
     return contact_countdown.days
     
-
-
-# write the logic that does the calculation. I need to know what today is vs. last modified + time delta
-# for loop that goes through list and then filters
 # TODO give the below a second integer input so that user can set their own threshold to be notified
 def get_contacts_due(contact_list):
+
     filtered = []
     for contact in contact_list:
         if get_contact_countdown(contact) <= 3: # && snooze is in the past
             filtered.append(contact)
     return filtered
-    #python has built in list filtering
             
 # TODO skip function
-
+    # eventually contacts would be tracked, and this would reset freq_modified w/out
+    # adding a record of a contact
     # contact due in contact_countdown days
-    # render in red if negative #    
 
-#*** add logic to edit_contact that triggers an update to frequency_modified when it's changed
-# we don;t actually need tz if everything is relative to utc
-# I'd rather save freq as integer
-
-# if you click snooze, time delta would be one day, last modified also updates, snooze adds a day at a time
-def save_snooze(): #onclick
-    snooze = datetime.now(timezone.utc) + timedelta(days=1)
-    return snooze
-
-#jMc pseudo
-# if you click contacted, last modified updates to today, time delta stays the same
-# you can change time delta at anytime, and it would update last modified
-# jMc end
-
-
-
+# TODO snooze, time delta would be one day to start, then user options
+# def save_snooze(): #onclick
+#     snooze = datetime.now(timezone.utc) + timedelta(days=1)
+#     return snooze
