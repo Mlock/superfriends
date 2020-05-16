f078124e36b1fac1b84089ac46b14c102b561110
Chris Moe <chris.moe@gmail.com>
2020-05-16 16:06:22 -0700

    code cleanup

--- superfriends/config/base.py
+++ superfriends/config/base.py
@@ -127,18 +127,11 @@ AUTH_USER_MODEL = 'accounts.User'
 
 
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-AWS_ACCESS_KEY_ID = 'AKIAT6N646UXKW6FTT5Q'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'superfriends-static'
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_LOCATION = 'static'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '/static'),
-# ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
